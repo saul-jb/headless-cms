0a91d1eeb58b76b46d0a2016c83cff848b60d83a
nutmmm <nutmmm@organicdesign.co.nz>
2018-08-02 16:19:32 +1200

    First user check hook.

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -2,23 +2,40 @@ const auth = require("@feathersjs/authentication");
 const local = require("@feathersjs/authentication-local");
 const checkPermissions = require("feathers-permissions");
 
+const firstUserCheck = require("../../hooks/first-user-check");
+
 // TODO: Seed the first user (admin)
 
 module.exports = {
 	before: {
-		all: [checkPermissions({ roles: ["users"] })],
-		find: [auth.hooks.authenticate("jwt")],
-		get: [auth.hooks.authenticate("jwt")],
-		create: [local.hooks.hashPassword({ passwordField: "password" })],
+		all: [],
+		find: [
+			auth.hooks.authenticate("jwt"),
+			checkPermissions({ roles: ["users"] })
+		],
+		get: [
+			auth.hooks.authenticate("jwt"),
+			checkPermissions({ roles: ["users"] })
+		],
+		create: [
+			local.hooks.hashPassword({ passwordField: "password" }),
+			firstUserCheck(), // Will skip any further hooks for the first user
+			checkPermissions({ roles: ["users"] })
+		],
 		update: [
 			auth.hooks.authenticate("jwt"),
-			local.hooks.hashPassword({ passwordField: "password" })
+			local.hooks.hashPassword({ passwordField: "password" }),
+			checkPermissions({ roles: ["users"] })
 		],
 		patch: [
 			auth.hooks.authenticate("jwt"),
-			local.hooks.hashPassword({ passwordField: "password" })
+			local.hooks.hashPassword({ passwordField: "password" }),
+			checkPermissions({ roles: ["users"] })
 		],
-		remove: [auth.hooks.authenticate("jwt")]
+		remove: [
+			auth.hooks.authenticate("jwt"),
+			checkPermissions({ roles: ["users"] })
+		]
 	},
 
 	after: {
